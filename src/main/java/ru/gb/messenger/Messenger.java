9a32cbe2b004102ef49ea72af783518caa7a9c99
Nikolay Uvarov <uvarov_na@mail.ru>
2022-04-17 18:32:09 +0300

    lesson2_4

---
+++ src/main/java/ru/gb/messenger/Messenger.java
@@ -0,0 +1,23 @@
+package ru.gb.messenger;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Messenger extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Messenger.class.getResource("main-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500, 400);
+        stage.setTitle("Messenger");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
